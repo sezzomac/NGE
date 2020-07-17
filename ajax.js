38b28f8902496e8daad52ebd4bf9a9f421c3b756
sezzomac <64476216+sezzomac@users.noreply.github.com>
2020-07-17 16:03:12 +0200

    update

--- ajax.js
+++ ajax.js
@@ -9,6 +9,32 @@ xhttp.onreadystatechange = function() {
 
 xhttp.open("GET", "mainsnippet.html", true);
 xhttp.send();
-//var element = document.getElementById("main-content");
-//element.innerHTML = xhttp.responseText;
+}
+
+
+function getProj() {
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+	if(this.readyState == 4 && this.status == 200) {
+		document.getElementById("main-content").innerHTML =
+		this.responseText;
+	}
+}
+
+xhttp.open("GET", "projects.html", true);
+xhttp.send();
+}
+
+
+function getAbout() {
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+	if(this.readyState == 4 && this.status == 200) {
+		document.getElementById("main-content").innerHTML =
+		this.responseText;
+	}
+}
+
+xhttp.open("GET", "aboutus.html", true);
+xhttp.send();
 }
